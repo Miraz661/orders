2c65601f28bcb07344a41190a01881066fa2a595
Miraz Hossain <105309402+Miraz661@users.noreply.github.com>
2024-05-30 11:05:20 +0600

    Delete userreg directory

--- userreg/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('dbconnect.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM user_details WHERE id=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: view.php"); 
-?>
